2476f68f5d16f4ae64754285d1a33d01098ed57a
31shal <102649832+31shal@users.noreply.github.com>
2022-04-02 18:02:25 +0530

    Update test_sequencer.sv

--- test_sequencer.sv
+++ test_sequencer.sv
@@ -1,6 +1,7 @@
-class m_sequencer extends uvm_sequencer;
+class m_sequencer extends uvm_sequencer#(test_transaction);
 uvm_component_utils(m_sequencer);
 function new(string_name ="m_sequencer", uvm_component parent = null);
   super.new(name,parent);
+  `uvm_update_sequence_lib_and_item(test_transaction)
 endfunction
 endclass
